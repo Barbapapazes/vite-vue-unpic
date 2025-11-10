5d94df923c0d9ec7afdcb060ab07f0a05ea5516b
Barbapapazes <e.soubiran25@gmail.com>
2025-11-10 16:49:01 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <img src="/steve-busch-tHfRemjLK4M-unsplash.jpg" />
+
+   <p>
+    Photo by Steve Busch on Unsplash
+   </p>
+  </div>
+</template>
+
+<style scoped>
+img {
+  width: 800px;
+  height: auto;
+}
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,54 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { MagicString } from 'vue/compiler-sfc'
+import { cwd } from 'node:process'
+import { join } from 'node:path'
+import { readFile } from 'node:fs/promises'
+import { encode } from "blurhash";
+import { getPixels } from '@unpic/pixels'
+import { blurhashToDataUri } from "@unpic/placeholder";
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    (() => {
+      const publicDir = join(cwd(), 'public')
+
+      return {
+        name: 'unpic',
+        enforce: 'pre',
+        async transform(code, id) {
+          if (!id.endsWith('.vue')) return
+
+          const s = new MagicString(code)
+
+          // extract src from img tags
+          const imgTagRegex = /<img\s+[^>]*src=["']([^"']+)["'][^>]*>/g
+          let match
+          while ((match = imgTagRegex.exec(code)) !== null) {
+            const srcValue = match[1]
+
+            const img = await readFile(join(publicDir, srcValue))
+            const data = await getPixels(img)
+            const blurhash = encode(Uint8ClampedArray.from(data.data), data.width, data.height, 4, 4)
+
+            const imgTagStart = match.index
+            const imgTagEnd = imgTagStart + match[0].length
+
+            const newImgTag = match[0].replace(
+              /<img(\s+)/,
+              `<img$1width="${data.width}" height="${data.height}" style="background-size: cover; background-image: url(${blurhashToDataUri(blurhash)});" loading="lazy" `
+            )
+
+            s.overwrite(imgTagStart, imgTagEnd, newImgTag)
+          }
+
+          return {
+            code: s.toString(),
+            map: s.generateMap({ hires: true })
+          }
+        }
+      }
+    })()
+  ],
+})
